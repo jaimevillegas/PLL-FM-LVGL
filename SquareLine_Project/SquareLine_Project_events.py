50a76b740db80022461641eca0fd4796c7f5ca86
Jaime Villegas <jaimevillegas296@gmail.com>
2023-04-19 16:44:35 -0500

    Add Squareline Project

---
+++ SquareLine_Project/SquareLine_Project_events.py
@@ -0,0 +1,20 @@
+
+def toggleLed(event_struct):
+    return
+
+
+def turnOnLed(event_struct):
+    return
+
+
+def turnOffLed(event_struct):
+    return
+
+
+def readPot(event_struct):
+    return
+
+
+def readPotIn(event_struct):
+    return
+
